4a5db11872492e4a9b7d4f83c2e68db9247ee2ac
Aditya Rathod <15635979+applecrazy@users.noreply.github.com>
2019-09-21 09:23:08 -0500

    Clean up stuff, add routes

---
+++ src/pages/TreatmentDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class TreatmentDetail extends Component {
+    render() {
+        return (
+            <p>hi</p>
+        )
+    }
+}
+
+export default TreatmentDetail
---
+++ src/state/state.js
@@ -0,0 +1,4 @@
+var defaultState = {
+    
+}
+export default defaultState
