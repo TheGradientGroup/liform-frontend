164863c4df214d6f8b5641883883593ca3f12769
Aditya Rathod <15635979+applecrazy@users.noreply.github.com>
2019-09-21 12:39:33 -0500

    Style Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,14 @@
 import React from "react"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import './Navbar.css'
 
 function Navbar() {
-    return(
-        <div>
-            <nav className="navbar">
-                <Link className="compare-costs" to="../pages/Home.js">
-                    Compare Costs
-                </Link>
-                <Link className="compare-costs" to="../pages/Home.js">
-                    Share Your Bill
-                </Link>
-                <div className="field">
-                  <div className="control">
-                    <input className="input" type="text" placeholder="Search for a treatment"/>
-                  </div>
-                </div>
-            </nav>
+    return (
+        <div className="liform-navbar">
+            <div className="container liform-nav-container">
+                <img src="logo.svg" className="liform-logo" />
+                <input type="text" className="input liform-top-search" placeholder="search for a treatment or DRG code" />
+            </div>
         </div>
     )
 }
