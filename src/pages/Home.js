726504d4b3164a19c41690706fd5e274d9d13e4e
Aditya Rathod <15635979+applecrazy@users.noreply.github.com>
2019-09-21 18:07:17 -0500

    Add new hero image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,11 @@ class Home extends Component {
   render () {
     return (
       <div className="home">
-        <div className="hero home-hero">
+        <div className="hero is-medium has-text-centered home-hero">
+          <div className="hero-body">
           <h1 className="home-title">Share and compare medical bills.</h1>
-          <h2 className="home-subtitle">Make major medical procedures more affordable.</h2>
+          <h2 className="home-subtitle">Liform makes major medical procedures more affordable.</h2>
+          </div>
         </div>
         <footer className="footer"></footer>
       </div>
