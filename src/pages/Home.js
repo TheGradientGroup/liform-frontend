e8b1a039fa59056099a45bf2edd82f1d60d1a67d
Aditya Rathod <15635979+applecrazy@users.noreply.github.com>
2019-09-21 19:38:21 -0500

    Add UI changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 import './Home.css'
 
 class Home extends Component {
   render () {
     return (
+      <>
       <div className="home">
         <div className="hero is-medium has-text-centered home-hero">
           <div className="hero-body">
@@ -11,8 +13,23 @@ class Home extends Component {
           <h2 className="home-subtitle">Liform makes major medical procedures more affordable.</h2>
           </div>
         </div>
-        <footer className="footer"></footer>
-      </div>
+        <section className="section">
+          <h1 className="title is-2 has-text-centered liform-desc">How Liform Works</h1>
+          <div className="liform-steps has-text-centered">
+            <div className="liform-step">1. Search for the medical procedure you’re looking to receive</div>
+            <div className="liform-step">2. We’ll show you how much the procedure might cost at hospitals nearby</div>
+            <div className="liform-step">3. Share your medical bills to help others</div>
+          </div>
+        </section>
+        </div>
+        <footer className="footer">
+          <div className="container has-text-centered">
+            &copy; 2019 Liform Labs.
+            <br />
+            <Link to="/import">Import data &rarr;</Link>
+          </div>
+        </footer>
+      </>
     )
   }
 }
