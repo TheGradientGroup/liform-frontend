dd0396ed6e9e8cebf1920523d344789b0d8d7467
Aditya Rathod <15635979+applecrazy@users.noreply.github.com>
2019-09-21 19:44:18 -0500

    This is a branch cluster.

--- src/pages/TreatmentDetail.js
+++ src/pages/TreatmentDetail.js
@@ -1,6 +1,81 @@
 import React, { Component } from 'react'
 
+class Result extends Component {
+
+    /**
+     * @param {Number} amount The price to convert into United States dollars.
+     */
+    formatCurrency(amount) {
+        return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(amount)
+    }
+
+    render() {
+        return (
+            <div className="hospital-desc">
+                <div className="columns">
+                    <div className="column has-text-left">
+                        <strong>{this.props.hospital.name}</strong>
+                        <p><em>{this.props.hospital.location}</em></p>
+                    </div>
+                    <div className="column has-text-right">{this.formatCurrency(this.props.hospital.price)}</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class ResultsList extends Component {
+    render() {
+        const items = this.props.results.map(result => <Result hospital={result} />)
+        return (
+            <>
+                <div className="columns">
+                    <div className="column">
+                        <br />
+                        <img src="https://via.placeholder.com/300" />
+                    </div>
+                    <div className="column">
+                        <input type="text" className="input" placeholder="search for a hospital..." />
+                        <div className="box">
+                            {items}
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
 class TreatmentDetail extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            treatmentName: 'Cartoid Artery Stent Procedure',
+            averageCost: 0,
+            results: [
+                {
+                    name: 'X Hospital',
+                    location: 'Richardson',
+                    price: 150000
+                },
+                {
+                    name: 'Y Hospital',
+                    location: 'Coppell, TX',
+                    price: 99000
+                }
+            ]
+        }
+    }
+
+    calculuateAverageCost(results) {
+        return results => results.reduce((a, b) => a.price + b, 0) / results.length
+    }
+
+    fetchResults(query) {
+        // TODO
+    }
+
     render() {
         return (
             <>
@@ -8,50 +83,18 @@ class TreatmentDetail extends Component {
                     <div className="container">
                         <div className="columns">
                             <div className="column">
-                                <h1 className="title">Cartoid Artery Stent Procedure</h1>
+                                <h1 className="title">{this.state.treatmentName}</h1>
                             </div>
                             <div className="column is-4" style={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', fontSize: '1.3rem' }}>
-                                Average cost: $109,003
-                           </div>
+                                Average cost: {this.calculuateAverageCost(this.state.results)}
+                            </div>
                         </div>
                     </div>
                 </section>
                 <section className="section">
                     <div className="container">
                         <h4 className="title is-4 has-text-centered">Hospitals Near You</h4>
-                        <div className="columns">
-                            <div className="column">
-                                MAP
-                                <br/>
-                                <img src="https://via.placeholder.com/300"/>
-                            </div>
-                            <div className="column">
-                                <input type="text" className="input" placeholder="search for a hospital..." />
-                                <br />
-                                <br/>
-                                <div className="box">
-                                    <div className="hospital-desc">
-                                        <div className="columns">
-                                            <div className="column has-text-left">
-                                                <strong>X Hospital</strong>
-                                                <p><em>Richardson, TX</em></p>
-                                            </div>
-                                            <div className="column has-text-right">$150,000</div>
-                                        </div>
-                                    </div>
-                                    <hr />
-                                    <div className="hospital-desc">
-                                        <div className="columns">
-                                            <div className="column has-text-left">
-                                                <strong>Y Hospital</strong>
-                                                <p><em>Coppell, TX</em></p>
-                                            </div>
-                                            <div className="column has-text-right">$99,000</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <ResultsList results={this.state.results} />
                         <div className="has-text-centered"><em>Did you get this procedure? Help others out and <a href="#">share your medical bill here &rarr;</a></em></div>
                     </div>
                 </section>
