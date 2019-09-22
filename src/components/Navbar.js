e47da23a93fe4f27dad53fa2c08c305281bd84fa
Aditya Rathod <15635979+applecrazy@users.noreply.github.com>
2019-09-22 01:39:27 -0500

    Add algolia

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,33 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import './Navbar.css'
+import { callbackify } from "util"
+import algoliasearch from 'algoliasearch/lite';
+import { connectAutoComplete } from 'react-instantsearch-dom';
+
+const searchClient = algoliasearch('ZL91PC3JWP', 'a50b8e6f848607e36fe279be76682f06');
 
 class Navbar extends Component {
     constructor() {
         super()
         this.state = {
-            searchText: ''
+            searchText: '',
+            searchSuggestions: [
+                { "drg": "001", "desc": "Cancer of the code" }
+            ],
+            displaySuggestions: false
         }
-    this.onSubmit = this.onSubmit.bind(this)
-    this.handleSearchTextChange = this.handleSearchTextChange.bind(this)
+        this.handleSearchTextChange = this.handleSearchTextChange.bind(this)
+        this.processSuggestion = this.processSuggestion.bind(this)
+    }
+
+    processSuggestion(e) {
+        console.log(e)
+        // this.setState({ searchText: this.state.searchSuggestions[idx].desc || '' })
+        console.log('hey')
     }
 
     handleSearchTextChange(e) {
-        this.setState({searchText: e.target.value}, () => {
+        this.setState({ searchText: e.target.value, displaySuggestions: true }, () => {
             console.log(this.state)
         })
     }
@@ -26,16 +41,17 @@ class Navbar extends Component {
     //       this.search(this.state.searchText);
     //     }
     // }
-  
-    search(query){
+
+    search(query) {
         fetch("https://liform-backend.herokuapp.com/search", {
             method: 'GET',
             body: {
                 query
             },
             headers: {
-              "Content-Type": "application/json"
-        }})
+                "Content-Type": "application/json"
+            }
+        })
             .then(response => response.json())
             .then(data => {
                 console.log(data)
@@ -45,21 +61,37 @@ class Navbar extends Component {
     }
 
     render() {
-        return(
+        var searchSugg = null
+        if (this.state.displaySuggestions) {
+            var sugArr = this.state.searchSuggestions.map((val, idx) => { 
+                return <option key={idx} value={val.desc} />
+            })
+            searchSugg = (
+                <datalist id="data">
+                    {sugArr}
+                </datalist>
+            )
+        }
+        return (
             <div className="liform-navbar">
                 <div className="container liform-nav-container">
-                    <img src="logo.svg" className="liform-logo" />
-                    <input 
-                        type="text"
+                    <img src="/logo.svg" className="liform-logo" />
+                    <input
+                        type="search"
+                        value={this.props.currentRefinement}
+                        onChange={event => this.props.refine(event.currentTarget.value)}
                         className="input liform-top-search"
-                        value={this.state.searchText}
-                        onChange={this.handleSearchTextChange}
+                        // value={this.state.searchText}
+                        // onChange={this.handleSearchTextChange}
+                        onFocus={() => this.setState({ displaySuggestions: true })}
+                        onBlur={() => this.setState({ displaySuggestions: false })}
                         placeholder="search for a treatment or DRG code"
                     />
+                    {searchSugg}
                 </div>
             </div>
         )
     }
 }
 
-export default Navbar
+export default connectAutoComplete(Navbar)
