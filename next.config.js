5f8dc5323a7ca55dfa12362d1acdd378abc85a10
Aditya Rathod <15635979+applecrazy@users.noreply.github.com>
2019-12-03 01:57:40 -0600

    Change env var storage

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 module.exports = {
     env: {
-        API_SERVER: process.env.API_SERVER,
-        IPSTACK_KEY: process.env.IPSTACK_KEY
+        API_SERVER: '@apiserver',
+        IPSTACK_KEY: '@ipstack-key'
     }
 };
